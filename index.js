f1c143bbb99bdc5428fc2e7d4a97eabe2e610f25
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-10-07 02:57:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// Leetcode 326 Power of Three
+// Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+// An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+// Input: n = 27
+// Output: true
+var isPowerOfThree = function(n) {
+
+
+}
